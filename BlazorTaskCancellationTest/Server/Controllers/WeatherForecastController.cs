3e294964b74debeda973d349dc7e4897fa7f957b
Quickz <alexserg@inbox.lv>
2022-02-06 21:35:52 +0200

    added the project

---
+++ BlazorTaskCancellationTest/Server/Controllers/WeatherForecastController.cs
@@ -0,0 +1,50 @@
+using BlazorTaskCancellationTest.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorTaskCancellationTest.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<List<WeatherForecast>> Get(CancellationToken token)
+        {
+            var data = new List<WeatherForecast>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                // Artifical delay
+                await Task.Delay(500);
+
+                if (token.IsCancellationRequested)
+                {
+                    token.ThrowIfCancellationRequested();
+                }
+
+                var instance = new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(i + 1),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                };
+
+                data.Add(instance);
+            }
+
+            return data;
+        }
+    }
+}
